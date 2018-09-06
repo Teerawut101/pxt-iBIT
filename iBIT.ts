e0ad4613eaf42db7280646395a2336f4d70db66c
Teerawut101 <Teerawut.j@outlook.com>
2018-09-06 10:48:19 +0700

    Update iBIT.ts

--- iBIT.ts
+++ iBIT.ts
@@ -97,7 +97,7 @@ namespace iBIT {
    /**Motor Block to drives motor forward and backward. The speed motor is adjustable between 0 to 100.
       * @param speed percent of maximum speed, eg: 50
       */
-    //% blockId="ibit_Motor1" block="Motor1 %motor|speed %speed"
+    //% blockId="ibit_Motor1" block="Motor %motor|speed %speed"
     //% speed.min=0 speed.max=100
     //% weight=95
     export function Motor1(Motor: motor, speed: number): void {  
